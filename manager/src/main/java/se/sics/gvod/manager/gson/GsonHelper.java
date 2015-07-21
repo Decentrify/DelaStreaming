d9fec83711f27dbe85dc056112006e5e85591e1c
Alex Ormenisan <ormenisan.adrian@gmail.com>
2015-07-21 09:35:46 +0200

    +version -> +all dependency version cleaned gitignore removed GSON - no longer used moved from junit.Assert to assert to remove junit unnecessary dependency

--- manager/src/main/java/se/sics/gvod/manager/gson/GsonHelper.java
+++ manager/src/main/java/se/sics/gvod/manager/gson/GsonHelper.java
@@ -1,42 +1,42 @@
-/*
- * Copyright (C) 2009 Swedish Institute of Computer Science (SICS) Copyright (C)
- * 2009 Royal Institute of Technology (KTH)
- *
- * GVoD is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- */
-package se.sics.gvod.manager.gson;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-/**
- * @author Alex Ormenisan <aaor@sics.se>
- */
-public class GsonHelper {
-
-    private static Gson gson = null;
-
-    public static GsonBuilder getGsonBuilder() {
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        return gsonBuilder;
-    }
-
-    public static Gson getGson() {
-        if (gson == null) {
-            gson = getGsonBuilder().create();
-        }
-        return gson;
-    }
-}
+///*
+// * Copyright (C) 2009 Swedish Institute of Computer Science (SICS) Copyright (C)
+// * 2009 Royal Institute of Technology (KTH)
+// *
+// * GVoD is free software; you can redistribute it and/or
+// * modify it under the terms of the GNU General Public License
+// * as published by the Free Software Foundation; either version 2
+// * of the License, or (at your option) any later version.
+// *
+// * This program is distributed in the hope that it will be useful,
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// * GNU General Public License for more details.
+// *
+// * You should have received a copy of the GNU General Public License
+// * along with this program; if not, write to the Free Software
+// * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// */
+//package se.sics.gvod.manager.gson;
+//
+//import com.google.gson.Gson;
+//import com.google.gson.GsonBuilder;
+//
+///**
+// * @author Alex Ormenisan <aaor@sics.se>
+// */
+//public class GsonHelper {
+//
+//    private static Gson gson = null;
+//
+//    public static GsonBuilder getGsonBuilder() {
+//        GsonBuilder gsonBuilder = new GsonBuilder();
+//        return gsonBuilder;
+//    }
+//
+//    public static Gson getGson() {
+//        if (gson == null) {
+//            gson = getGsonBuilder().create();
+//        }
+//        return gson;
+//    }
+//}
