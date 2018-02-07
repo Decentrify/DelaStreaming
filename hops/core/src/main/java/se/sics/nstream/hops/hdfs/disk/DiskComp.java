59dac8312ae1d8f836d689a0b9e59fb3a0a79178
Alex Ormenisan <ormenisan.adrian@gmail.com>
2018-02-07 08:41:33 +0100

    streams should be able to create folders/file if they do not exist.

--- hops/core/src/main/java/se/sics/nstream/hops/hdfs/disk/DiskComp.java
+++ hops/core/src/main/java/se/sics/nstream/hops/hdfs/disk/DiskComp.java
@@ -48,147 +48,152 @@ import se.sics.nstream.storage.durable.util.StreamResource;
  */
 public class DiskComp extends ComponentDefinition {
 
-    private final static Logger LOG = LoggerFactory.getLogger(DiskComp.class);
-    private String logPrefix = "";
+  private final static Logger LOG = LoggerFactory.getLogger(DiskComp.class);
+  private String logPrefix = "";
 
-    Positive<Timer> timerPort = requires(Timer.class);
-    private final Negative storagePort = provides(DStoragePort.class);
-    //**************************************************************************
-    private final Identifier self;
-    //**************************************************************************
-    private RandomAccessFile raf;
-    private long writePos;
+  Positive<Timer> timerPort = requires(Timer.class);
+  private final Negative storagePort = provides(DStoragePort.class);
+  //**************************************************************************
+  private final Identifier self;
+  //**************************************************************************
+  private RandomAccessFile raf;
+  private long writePos;
 
-    public DiskComp(Init init) {
-        self = init.self;
-        logPrefix = "<nid:" + self.toString() + ">disk:" + init.filePath + " ";
-        LOG.info("{}init", logPrefix);
+  public DiskComp(Init init) {
+    self = init.self;
+    logPrefix = "<nid:" + self.toString() + ">disk:" + init.filePath + " ";
+    LOG.info("{}init", logPrefix);
 
-        raf = init.raf;
-        writePos = init.writePos;
+    raf = init.raf;
+    writePos = init.writePos;
 
-        subscribe(handleStart, control);
-        subscribe(handleRead, storagePort);
-        subscribe(handleWrite, storagePort);
+    subscribe(handleStart, control);
+    subscribe(handleRead, storagePort);
+    subscribe(handleWrite, storagePort);
+  }
+
+  Handler handleStart = new Handler<Start>() {
+    @Override
+    public void handle(Start event) {
+      LOG.info("{}starting", logPrefix);
     }
+  };
 
-    Handler handleStart = new Handler<Start>() {
-        @Override
-        public void handle(Start event) {
-            LOG.info("{}starting", logPrefix);
-        }
-    };
+  @Override
+  public void tearDown() {
+    LOG.info("{}tearing down", logPrefix);
+  }
+
+  Handler handleRead = new Handler<DStorageRead.Request>() {
+    @Override
+    public void handle(DStorageRead.Request req) {
+      LOG.debug("{}read:{}", logPrefix, req);
+      int readLength = (int) (req.readRange.upperAbsEndpoint() - req.readRange.lowerAbsEndpoint() + 1);
+      byte[] readVal = new byte[readLength];
+      int readPos = (int) req.readRange.lowerAbsEndpoint();
+      try {
+        LOG.debug("{}reading at pos:{} amount:{}", new Object[]{logPrefix, readPos, readLength, raf.length()});
+        raf.seek(readPos);
+        raf.readFully(readVal);
+      } catch (IOException ex) {
+        answer(req, req.respond(Result.internalFailure(ex)));
+      }
+      answer(req, req.respond(Result.success(readVal)));
+    }
+  };
+
+  Handler handleWrite = new Handler<DStorageWrite.Request>() {
+    @Override
+    public void handle(DStorageWrite.Request req) {
+      LOG.debug("{}write:{}", logPrefix, req);
+      if (writePos >= req.pos + req.value.length) {
+        LOG.debug("{}write with pos:{} skipped", logPrefix, req.pos);
+        answer(req, req.respond(Result.success(true)));
+        return;
+      }
+      long pos = req.pos;
+      byte[] writeValue = req.value;
+      if (writePos > req.pos) {
+        pos = writePos;
+        int sourcePos = (int) (pos - writePos);
+        int writeAmount = req.value.length - sourcePos;
+        writeValue = new byte[writeAmount];
+        System.arraycopy(req.value, sourcePos, writeValue, 0, writeAmount);
+        LOG.debug("{}convert write pos from:{} to:{} write amount from:{} to:{}",
+          new Object[]{logPrefix, req.pos, pos, req.value.length, writeAmount});
+      }
+      try {
+        raf.seek(req.pos);
+        raf.write(req.value);
+      } catch (IOException ex) {
+        answer(req, req.respond(Result.internalFailure(ex)));
+      }
+      writePos += writeValue.length;
+      answer(req, req.respond(Result.success(true)));
+    }
+  };
+
+  public static class Init extends se.sics.kompics.Init<DiskComp> {
+
+    public final Identifier self;
+    public final String filePath;
+    public final RandomAccessFile raf;
+    public final long writePos;
+
+    public Init(Identifier self, String filePath, RandomAccessFile raf, long writePos) {
+      this.self = self;
+      this.filePath = filePath;
+      this.raf = raf;
+      this.writePos = writePos;
+    }
+  }
+
+  public static class StorageProvider implements DurableStorageProvider<DiskComp> {
+
+    public final Identifier self;
+    public final DiskEndpoint endpoint = new DiskEndpoint();
+
+    public StorageProvider(Identifier self) {
+      this.self = self;
+    }
 
     @Override
-    public void tearDown() {
-        LOG.info("{}tearing down", logPrefix);
+    public Pair<Init, Long> initiate(StreamResource resource) {
+      try {
+        DiskResource diskResource = (DiskResource) resource;
+        File dir = new File(diskResource.dirPath);
+        if (!dir.exists()) {
+          dir.mkdirs();
+        }
+        String filePath = diskResource.dirPath + File.separator + diskResource.fileName;
+        File file = new File(filePath);
+        if (!file.exists()) {
+          file.createNewFile();
+        }
+        RandomAccessFile raf = new RandomAccessFile(filePath, "rw");
+        long filePos = raf.length();
+        Init init = new Init(self, filePath, raf, filePos);
+      return Pair.with(init, filePos);
+      } catch (FileNotFoundException ex) {
+        throw new RuntimeException(ex);
+      } catch (IOException ex) {
+        throw new RuntimeException(ex);
+      }
     }
 
-    Handler handleRead = new Handler<DStorageRead.Request>() {
-        @Override
-        public void handle(DStorageRead.Request req) {
-            LOG.debug("{}read:{}", logPrefix, req);
-            int readLength = (int) (req.readRange.upperAbsEndpoint() - req.readRange.lowerAbsEndpoint() + 1);
-            byte[] readVal = new byte[readLength];
-            int readPos = (int) req.readRange.lowerAbsEndpoint();
-            try {
-                LOG.debug("{}reading at pos:{} amount:{}", new Object[]{logPrefix, readPos, readLength, raf.length()});
-                raf.seek(readPos);
-                raf.readFully(readVal);
-            } catch (IOException ex) {
-                answer(req, req.respond(Result.internalFailure(ex)));
-            }
-            answer(req, req.respond(Result.success(readVal)));
-        }
-    };
-
-    Handler handleWrite = new Handler<DStorageWrite.Request>() {
-        @Override
-        public void handle(DStorageWrite.Request req) {
-            LOG.debug("{}write:{}", logPrefix, req);
-            if (writePos >= req.pos + req.value.length) {
-                LOG.debug("{}write with pos:{} skipped", logPrefix, req.pos);
-                answer(req, req.respond(Result.success(true)));
-                return;
-            }
-            long pos = req.pos;
-            byte[] writeValue = req.value;
-            if (writePos > req.pos) {
-                pos = writePos;
-                int sourcePos = (int) (pos - writePos);
-                int writeAmount = req.value.length - sourcePos;
-                writeValue = new byte[writeAmount];
-                System.arraycopy(req.value, sourcePos, writeValue, 0, writeAmount);
-                LOG.debug("{}convert write pos from:{} to:{} write amount from:{} to:{}",
-                        new Object[]{logPrefix, req.pos, pos, req.value.length, writeAmount});
-            }
-            try {
-                raf.seek(req.pos);
-                raf.write(req.value);
-            } catch (IOException ex) {
-                answer(req, req.respond(Result.internalFailure(ex)));
-            }
-            writePos += writeValue.length;
-            answer(req, req.respond(Result.success(true)));
-        }
-    };
-
-    public static class Init extends se.sics.kompics.Init<DiskComp> {
-
-        public final Identifier self;
-        public final String filePath;
-        public final RandomAccessFile raf;
-        public final long writePos;
-
-        public Init(Identifier self, String filePath, RandomAccessFile raf, long writePos) {
-            this.self = self;
-            this.filePath = filePath;
-            this.raf = raf;
-            this.writePos = writePos;
-        }
+    @Override
+    public String getName() {
+      return endpoint.getEndpointName();
     }
 
-    public static class StorageProvider implements DurableStorageProvider<DiskComp> {
-
-        public final Identifier self;
-        public final DiskEndpoint endpoint = new DiskEndpoint();
-
-        public StorageProvider(Identifier self) {
-            this.self = self;
-        }
-
-        @Override
-        public Pair<Init, Long> initiate(StreamResource resource) {
-            DiskResource diskResource = (DiskResource) resource;
-            String filePath = diskResource.dirPath + File.separator + diskResource.fileName;
-            RandomAccessFile raf;
-            long filePos;
-            try {
-                raf = new RandomAccessFile(filePath, "rw");
-                filePos = raf.length();
-            } catch (FileNotFoundException ex) {
-                throw new RuntimeException(ex);
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
-
-            Init init = new Init(self, filePath, raf, filePos);
-            return Pair.with(init, filePos);
-        }
-
-        @Override
-        public String getName() {
-            return endpoint.getEndpointName();
-        }
-
-        @Override
-        public Class<DiskComp> getStorageDefinition() {
-            return DiskComp.class;
-        }
-
-        @Override
-        public StreamEndpoint getEndpoint() {
-            return endpoint;
-        }
+    @Override
+    public Class<DiskComp> getStorageDefinition() {
+      return DiskComp.class;
     }
+
+    @Override
+    public StreamEndpoint getEndpoint() {
+      return endpoint;
+    }
+  }
 }
